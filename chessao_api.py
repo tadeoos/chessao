e545e673e5298535714741679138eeda88aaa7f5
Tadek Teleżyński <tadekte@gmail.com>
2017-04-11 23:17:25 +0200

    add new

---
+++ chessao_api.py
@@ -0,0 +1,7 @@
+import szachao, roz2
+
+
+
+def get_moves(fen, start, end):
+	b = szachao.board(fenrep=fen)
+	
---
+++ setup.py
@@ -0,0 +1,5 @@
+import szachao, roz2
+
+t = szachao.Talia()
+b = szachao.board()
+r = roz2.rozgrywka()
