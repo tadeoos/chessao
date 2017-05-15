76c1c05535b50c811fb771ef0ff56366b4f7ec60
Tadek Teleżyński <tadekte@gmail.com>
2017-05-15 12:26:06 +0200

    add first simple unittests

--- src/szachao_testy.py
+++ src/szachao_testy.py
@@ -1,22 +1,55 @@
-import coverage, doctest
-
-
-if __name__ == "__main__":
-	cov = coverage.coverage(branch=True)
-	cov.erase()
-	cov.start()
-	import testy2
-	import roz2,szachao
-	doctest.testmod(szachao)
-	doctest.testmod(roz2)
-	cov.stop()
-	modules=[szachao,roz2]
-	cov.report(modules, ignore_errors=1, show_missing=1)
-	cov.html_report(morfs=modules, directory='/tmp')
-	cov.erase()
+import coverage
+import doctest
+import unittest
+import time
 
 # jakie testy?
 # 1. Czy dane pole NIE jest w możliwym zakresie
 # 2. Czy dane pole JEST w możliwym zakresie
 # 3. Czy dana figura JEST/NIE MA w możliwym zakresie...
 # Pustość ruchu
+
+
+def game(rnd=0):
+    game = roz2.rozgrywka(rnd)
+    game.graj(rnd)
+    return game
+
+
+class TestGame(unittest.TestCase):
+    import roz2
+
+    def setUp(self):
+        self.roz = roz2.rozgrywka(0)
+
+    def test_10_runs(self):
+        for i in range(2):
+            g = game()
+            print(g.historia)
+        self.assertFalse(game().szach)
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == "__main__":
+    t1 = time.time()
+    cov = coverage.coverage(branch=True)
+    cov.erase()
+    cov.start()
+    import testy2
+    import roz2
+    import szachao
+    doctest.testmod(szachao)
+    doctest.testmod(roz2)
+    cov.stop()
+    modules = [szachao, roz2]
+    cov.report(modules, ignore_errors=1, show_missing=1)
+    cov.html_report(morfs=modules, directory='/tmp')
+    cov.erase()
+    t2 = time.time()
+    print('TESTS TOOK TIME: {:.2f} min'.format((t2 - t1) / 60))
+    unittest.main()
--- src/testy2.py
+++ src/testy2.py
@@ -1,39 +1,39 @@
-import szachao, roz2
+# import szachao, roz2
 
-cases = [('8/P7/8/8/8/8/8/8',(1,'4')),('k1K5/8/8/8/8/8/8/8',(3,'K')), ('Wdg5/8/8/8/8/8/8/8',(1,'J'))]
+# cases = [('8/P7/8/8/8/8/8/8',(1,'4')),('k1K5/8/8/8/8/8/8/8',(3,'K')), ('Wdg5/8/8/8/8/8/8/8',(1,'J'))]
 
-def pos_moves(fen,card):
-	c = szachao.karta(card[0],card[1])
-	return roz2.rozgrywka(fenrep=fen, ovr=([c],[])).possible_moves(roz.to_move,roz.capture,roz.gracze[0].reka[0],roz.burned,roz.what_happened())
+# def pos_moves(fen,card):
+# 	c = szachao.karta(card[0],card[1])
+# return roz2.rozgrywka(fenrep=fen,
+# ovr=([c],[])).possible_moves(roz.to_move,roz.capture,roz.gracze[0].reka[0],roz.burned,roz.what_happened())
 
 
-def seq_of_moves(boot, mvs):
-	r = roz2.rozgrywka(fenrep=boot[0], ovr=[boot[1][0],boot[1][1]], test=True)
-	# print(r)
-	for card,move in mvs:
-		c = r.get_card(ovr=card)
-		m = r.get_move(ovr=move)
-		r.move(r.now_card,m)
-	return r
+# def seq_of_moves(boot, mvs):
+# 	r = roz2.rozgrywka(fenrep=boot[0], ovr=[boot[1][0],boot[1][1]], test=True)
+# 	# print(r)
+# 	for card,move in mvs:
+# 		c = r.get_card(ovr=card)
+# 		m = r.get_move(ovr=move)
+# 		r.move(r.now_card,m)
+# 	return r
 
-k = roz2.karta_z_str
-roz = seq_of_moves(('Wdg5/8/8/8/8/8/8/8',[[k('J1'),k('Q1'),k('51'),k('61'),k('81')],[k('101'),k('Q2'),k('K1'),k('62'),k('82')]]), (((0, [k('J1')], 'dama'), ['A1', 'B1']),((0,[k('K1')]),[]),))
+# k = roz2.karta_z_str
+# roz = seq_of_moves(('Wdg5/8/8/8/8/8/8/8',[[k('J1'),k('Q1'),k('51'),k('61'),k('81')],[k('101'),k('Q2'),k('K1'),k('62'),k('82')]]), (((0, [k('J1')], 'dama'), ['A1', 'B1']),((0,[k('K1')]),[]),))
 
-for i in range(1):
-	r = roz2.rozgrywka()
-	r.graj()
+# for i in range(1):
+# 	r = roz2.rozgrywka()
+# 	r.graj()
 
-def test1():
-	''' to jest dokumentacja 
-	a to jest druga linijka
+# def test1():
+# 	''' to jest dokumentacja
+# 	a to jest druga linijka
 
-	>>> test1()
-	2
-	>>> test1()
-	2
-	'''
-	for frep,card in cases:
-		roz = roz2.rozgrywka(fenrep=frep, ovr=([card],[]))
+# 	>>> test1()
+# 	2
+# 	>>> test1()
+# 	2
+# 	'''
+# 	for frep,card in cases:
+# 		roz = roz2.rozgrywka(fenrep=frep, ovr=([card],[]))
 
-		posmoves = roz
-
+# 		posmoves = roz
