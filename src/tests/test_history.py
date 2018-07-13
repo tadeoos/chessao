daccdf1a36440ef96a0f97dad4b9277b464511ab
Tadek Teleżyński <tadekte@gmail.com>
2018-07-13 04:03:06 +0200

    Add simple test_get_move_from_turn for history module

--- src/tests/test_history.py
+++ src/tests/test_history.py
@@ -81,3 +81,6 @@ class TestHistory:
             'halfmove': 0,
             'full_move': 20
         }
+
+    def test_get_move_from_turn(self, chessao_four_played):
+        assert chessao_four_played.history.get_move_from_turn(1, 'card') == Card(1, '4')
