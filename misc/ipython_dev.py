8ec560caf93b4015dbc115c644aa6630dd5fb85d
Tadek Teleżyński <tadekte@gmail.com>
2018-07-27 13:31:53 +0200

    add more imports to dev

--- misc/ipython_dev.py
+++ misc/ipython_dev.py
@@ -1,11 +1,13 @@
 import imp
 
-from chessao import WHITE_COLOR, BLACK_COLOR
-from chessao.cards import Card, ChessaoCards
+from chessao import WHITE_COLOR, BLACK_COLOR, MAPDICT
+from chessao.cards import Card, ChessaoCards, Deck
 from chessao.chess import Board
 from chessao.pieces import Rook, Bishop, King, Knight, Queen
 from chessao.players import Player
 from chessao.gameplay import ChessaoGame
+from chessao.history import ChessaoHistory
+from tests.utils import load_game_pkl_from, load_simulation_bugs
 
 DEFAULT_PLAYERS = (
     Player(1, WHITE_COLOR, name='white'),
